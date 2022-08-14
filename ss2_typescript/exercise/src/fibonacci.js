01bbc621d9326446d73bbc673b1f171f5829fbd2
tranvanhai591989 <tranvanhai591989@gmail.com>
2022-08-14 11:12:55 +0700

    first commit

--- ss2_typescript/exercise/src/fibonacci.js
+++ ss2_typescript/exercise/src/fibonacci.js
@@ -5,6 +5,7 @@ function fibonacci(num) {
         return 1;
     return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
 var sum = 0;
 for (var i = 0; i < 10; i++) {
     console.log("The Number Of  " + (i + 1) + " Is :" + fibonacci(i));
