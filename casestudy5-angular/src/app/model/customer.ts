ce0805d93323b222c163f4f06f24d5cf075f6c63
tranvanhai591989 <tranvanhai591989@gmail.com>
2022-08-17 08:17:01 +0700

    up

---
+++ casestudy5-angular/src/app/model/customer.ts
@@ -0,0 +1,11 @@
+export interface Customer {
+  id?: number;
+  name?: string;
+  birthday?: string;
+  gender?: string;
+  idCard?: number;
+  phone?: number;
+  email?: string;
+  address?: string;
+  customerType?: string;
+}
---
+++ casestudy5-angular/src/app/model/facility.ts
@@ -0,0 +1,9 @@
+export interface Facility {
+  id?: number;
+  name?: string;
+  area?: number;
+  rentalCost?: number;
+  maxPeople?: number;
+  rentalType?: string;
+  image?: string;
+}
