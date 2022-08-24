ba41374758047a4d5b99a854660499d781113ba6
tranvanhai591989 <tranvanhai591989@gmail.com>
2022-08-24 16:07:06 +0700

    up

---
+++ ss8_modules_di/angular-gallery-app/src/app/image-gallery/image-gallery.component.ts
@@ -0,0 +1,33 @@
+import {Component, Inject, OnInit, Optional} from '@angular/core';
+import {GalleryConfig} from './token';
+
+@Component({
+  selector: 'app-image-gallery',
+  templateUrl: './image-gallery.component.html',
+  styleUrls: ['./image-gallery.component.css']
+})
+export class ImageGalleryComponent implements OnInit {
+  listImage = [
+    'https://i.pinimg.com/564x/50/5b/52/505b5207c97d90a36e64aad2d5ebfc10.jpg',
+    'https://i.pinimg.com/564x/c1/36/9c/c1369cea824585999a761e31040c6bd2.jpg',
+    'https://i.pinimg.com/564x/50/5b/52/505b5207c97d90a36e64aad2d5ebfc10.jpg',
+    'https://i.pinimg.com/564x/da/b8/aa/dab8aab671ebc7c699b9d6919143d8bb.jpg'
+  ];
+  itemWidth: number;
+  config = 1;
+  p = 1;
+
+  constructor(
+    @Inject(GalleryConfig)
+    @Optional()
+      config: number
+  ) {
+    if (config) {
+      this.config = config;
+    }
+  }
+
+  ngOnInit() {
+    this.itemWidth = 100 / this.config;
+  }
+}
